393d0fd4eeb5bac74c6e7b1e7eb128aff9efb32f
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-07-28 10:21:48 -0300

    starting the project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
